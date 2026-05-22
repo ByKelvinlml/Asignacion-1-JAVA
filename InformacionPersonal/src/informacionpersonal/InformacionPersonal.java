dab9890fe729a429ef241b3bed5b0b04e1792c95
ByKelvinlml <KELVINGV98@GMAIL.COM>
2026-05-22 18:44:31 -0400

    Add files via upload

---
+++ InformacionPersonal/src/informacionpersonal/InformacionPersonal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package informacionpersonal;
+
+public class InformacionPersonal {
+    public static void main(String[] args) {
+        String nombre = "Kelvin";
+        
+        
+        System.out.println(nombre);
+        System.out.println(nombre + "gv98@gmail.com");
+    }
+    
+}
